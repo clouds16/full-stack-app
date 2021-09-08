5216f3c1ee2b7a5412947393dbd12178a474e9fa
hect <hect16@gmail.com>
2021-09-08 18:43:22 -0500

    front end back end connevtivity

--- backend/app.js
+++ backend/app.js
@@ -2,25 +2,21 @@ const express = require("express");
 // require("./mongoose.js");
 // const User = require("./models/user");
 const cors = require('cors');
-const request = require('request')
+const axios = require('axios')
 
 const app = express();
 const port = process.env.PORT || 5000;
+
 app.use(express.json());
 app.use(cors());
 
-
-
-app.post("/", (req, res) => {
-    let url = "https://api.darksky.net/forecast/90cdda4c36fb9e5ea8b786c7034713f9/37.8267,-122.4233";
-    request(url , (err, res , body ) => {
-        if (!err) {
-            res.send(body);
-            console.log(body)
-        }
-    })
-
-
+app.get('/data', (req, res) =>{
+    let url =   "https://api.darksky.net/forecast/90cdda4c36fb9e5ea8b786c7034713f9/37.8267,-122.4233";
+    axios.get(url).then( (response)=>{
+        console.log(response.data.currently)
+        const temp = JSON.parse(response.data.currently['temperature'])
+        res.send( { temp })
+    }).catch(e => res.send(e))
 })
 
 
--- frontendd/src/App.js
+++ frontendd/src/App.js
@@ -1,28 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react'
 import axios from 'axios'
-
+import {useState} from 'react'
 
 function App() {
 
-  let [weather , setWeather] = useState('No Weather Yet')
+  let [weather , setWeather] = useState('No data yet')
 
-  function getWeather(){
-    axios.post("/" ).then(res=> {
-      console.log(res)
-    })
 
-    
+  function getData() {
+    axios.get('/data').then( (response) =>{
+
+      console.log(response.data.temp)
+
+      setWeather(response.data.temp)
+    } ).catch( (e) => console.log(e))
+
   }
 
 
   return (
     <div>
-      <button onClick={getWeather}> Get Weather </button>
-      <h1> The weather is currently: {weather}</h1>
-    </div>
-  )
+        
+        <h2> This is the data : {weather} </h2>
+        <button onClick={getData}> Click me to get data</button>
+    
+    </div> )
 }
 
 export default App;
