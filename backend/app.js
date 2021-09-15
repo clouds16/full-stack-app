b70a2bf5d523955f8cdffeaf5134b9609bf802e2
hect <hect16@gmail.com>
2021-09-14 22:03:37 -0500

    crypto coin section

--- backend/app.js
+++ backend/app.js
@@ -10,36 +10,28 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-app.get('/data', (req, res) =>{
-    const key = 'e07b291785207b5575a4ffc1db8ceac3';
-    //  api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-    let url =   "https://api.darksky.net/forecast/90cdda4c36fb9e5ea8b786c7034713f9/37.8267,-122.4233";
-    axios.get(url).then( (response)=>{
-        console.log(response.data.currently)
-        const temperature = JSON.parse(response.data.currently['temperature'])
-        const currently = response.data.currently
-        res.send( {currently} )
-
-    }).catch(e => res.send(e))
-})
-
 app.post('/weather' , (req, res) => {
-    
     let city = req.body.cityname
     const apikey = 'e07b291785207b5575a4ffc1db8ceac3'
     let url = "https://api.openweathermap.org/data/2.5/weather?q="+ city +  "&appid=" + apikey;
 
     axios.get(url).then( (response)=> {
-        let temperature = response.data.main.temp
-        res.send({temperature})
-
+        console.log(response)
+        res.send( response.data)
     }).catch((e)=>{
-        console.log(e)
+        console.log(e) 
         res.send(e)
     })
-
 })
 
+app.get('/cryptos', (req , res)=> {
+    const url = 'https://api.coinbase.com/v2/currencies'
+
+    axios.get(url).then( (response) =>{
+        console.log(response.data.data)
+        res.send(response.data.data)
+    }).catch ( e => res.send(e))
+} )
 
 app.listen(port, () => {
 	console.log("Server up on port: " + port);
--- frontendd/src/pages/cryptos.js
+++ frontendd/src/pages/cryptos.js
@@ -1,11 +1,58 @@
-import React from 'react'
+
+import Axios from 'axios';
+import  React , {useState , useEffect} from 'react';
+
+import '../static-elements/cryptos.css'
 
 function Cryptos(){
+
+    let [data , setData] = useState([])
+    let [check , setCheck] = useState(false)
+    
+  
+
+    function getData(e) {
+        if(!check) {
+            Axios.get('/cryptos').then( (res) => {
+                console.log(res.data)
+                setData(...data, res.data)
+                
+                console.log('the data is', data)
+            }).catch( (err) => {
+                console.log(err)
+            })
+        }
+        e.preventDefault();
+        setCheck( true)
+    }
+
+
+
     return (
-        <div>
-            <h2> Cryptos page </h2>    
-        </div>
+        <section className="cryptos-section">
+            
+            <h2> Cryptos page </h2>   
+            <button onClick={getData}>  Get List of Cryptos </button> 
+
+            <div className="flex "> 
+                <div className="mygrid">
+                { data.map( crypto => 
+                    <div className="box item">
+                        <h5> {crypto.name} : {crypto.id} </h5>
+                    </div>)}
+                </div>
+            </div>
+        </section>
     )
 }
 
 export default Cryptos;
+
+// {Object.keys(data).map((key, value) =>{
+//     return (
+//         <div>
+//             <p> name : {key.name} </p>
+//             <p> symbol : {key.symbol} </p>
+//         </div>
+//     )
+// })}
